b4170fcc61356fcf48db44819142fa70b52ea7bd
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-28 08:18:24 -0300

    Raiz do projeto

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-<?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-require './config/connect.php';
-require './app/controllers/ClienteController.php';
-
-use App\Controllers\ClienteController;
-
-$database = new Connect();
-
-$db = $database->connection;
-
-$clienteController = new ClienteController($db);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $clienteController->create();
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+    <header>
+        <h1>Página Inicial</h1>
+        <nav>
+            <ul>
+                <li><a href="./app/views/clientes/create.php">Cadastrar</a></li>
+                <li><a href="./app/views/clientes/listClientes.php">Clientes</a></li>
+                <li><a href="./app/views/clientes/delete.php">Deletar</a></li>
+                <li><a href="./app/views/clientes/update.php">Editar</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
