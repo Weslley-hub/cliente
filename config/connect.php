7cf923e0227b008946555c59db9b6991650e1096
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-28 08:16:37 -0300

    Conexão com o BD com PDO e POSTGREES

--- config/connect.php
+++ config/connect.php
@@ -15,7 +15,7 @@ class Connect {
    function connectDatabase() {
       try {
          $this->connection = new PDO('pgsql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
-         echo 'Sucesso, conectado ao banco de dados';
+         
       } catch (PDOException $e) {
          echo "Error".$e->getMessage();
          die();
@@ -25,20 +25,4 @@ class Connect {
 
 $testConnection = new Connect();
 
-// $db_name = 'pg';
-// $db_host = 'bd';
-// $db_user = 'root';
-// $db_password = 'root';
- 
-//  try {
-
-//     $pdo = new PDO("pgsql:host=$db_host;port=5432;dbname=$db_name", $db_user, $db_password);
-
-//     echo "Conectado no banco de dados";
-
-//  } catch (PDOException $e) {
-
-//     echo "Falha ao conectar. <br/>";
-//     die($e->getMessage());
-
-//  }
+?>
