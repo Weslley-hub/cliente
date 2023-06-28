db5fffd38b7325854db3ee47b7d56fc0a8da4aa5
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-28 08:15:29 -0300

    form de criar

--- app/views/clientes/create.php
+++ app/views/clientes/create.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Cadastrar Cliente</title>
+    <link rel="stylesheet" href="../styles/create.css">
 </head>
 <body>
     <h1>Cadastrar Cliente</h1>
-    <form method="post" action="/index.php">
+    <form method="post" action="../../../public/cadastrar/cadastrar.php">
+        <input type="hidden" name="action" value="create">
+
         <label for="nome">Nome:</label>
         <input type="text" name="nome" required><br>
 
@@ -19,3 +25,7 @@
     </form>
 </body>
 </html>
+
+
+
+
